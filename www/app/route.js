a682f7f7421620c7f5376150def063d4de39eb75
Emmanuel <emmanuel-ms@hotmail.com>
2016-01-07 10:32:24 -0600

    Change login form to be compliant with mocks

--- www/app/route.js
+++ www/app/route.js
@@ -8,51 +8,37 @@
     function setupRoutes($stateProvider, $urlRouterProvider) {
 
       $stateProvider
-        .state('tabs', {
-          url: '/tabs',
-          abstract: true,
-          templateUrl: 'app/menu/non-auth-tabs.html'
-        })
-        .state('tabs.login', {
-          url: '/login',
-          views: {
-            'login': {
-              templateUrl: 'app/login/view.html',
-              controller: 'LoginCtrl',
-              controllerAs: 'vm'
-            }
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/view.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'vm'
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'app/register/view.html',
+        controller: 'RegisterCtrl',
+        controllerAs: 'vm'
+      })
+      .state('side', {
+        url: '/side',
+        abstract: true,
+        templateUrl: 'app/menu/auth-side-menu.html',
+        controller: 'SideMenuCtrl',
+        controllerAs: 'vm'
+      })
+      .state('side.home', {
+        url: '/home',
+        views: {
+          'menuContent': {
+            templateUrl: 'app/home/view.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'vm'
           }
-        })
-        .state('tabs.register', {
-          url: '/register',
-          views: {
-            'register': {
-              templateUrl: 'app/register/view.html',
-              controller: 'RegisterCtrl',
-              controllerAs: 'vm'
-            }
-          }
-        })
-
-        .state('side', {
-          url: '/side',
-          abstract: true,
-          templateUrl: 'app/menu/auth-side-menu.html',
-          controller: 'SideMenuCtrl',
-          controllerAs: 'vm'
-        })
-        .state('side.home', {
-          url: '/home',
-          views: {
-            'menuContent': {
-              templateUrl: 'app/home/view.html',
-              controller: 'HomeCtrl',
-              controllerAs: 'vm'
-            }
-          }
-        });
-
-      $urlRouterProvider.otherwise('/tabs/login');
+        }
+      });
+      
+      $urlRouterProvider.otherwise('/login');
 
     }
 })();
