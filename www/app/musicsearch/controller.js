fab1d3dd54ceabfc6f17962cbabf7575f4b761ff
Emmanuel <emmanuel-ms@hotmail.com>
2016-02-12 14:58:46 -0600

    Add youtube search

--- www/app/musicsearch/controller.js
+++ www/app/musicsearch/controller.js
@@ -5,10 +5,29 @@
     .module('musicHack.musicsearch')
     .controller('MusicSearchCtrl', MusicSearchCtrl);
 
-  MusicSearchCtrl.$inject = [];
+  MusicSearchCtrl.$inject = ['SearchService', '$ionicPopup'];
 
   /* @ngInject */
-  function MusicSearchCtrl() {
+  function MusicSearchCtrl(SearchService, $ionicPopup) {
     var vm = this;
+    vm.videos = {};
+    vm.search = search;
+
+    function search() {
+      SearchService.searchYoutubeAPI(vm.searchQuery)
+        .then(setVideos)
+        .catch(handleSearchFailure);
+    }
+
+    function setVideos(result) {
+      vm.videos = result.data.items;
+    }
+
+    function handleSearchFailure(error) {
+      $ionicPopup.alert({
+        title: 'Ha ocurrido un error',
+        template: error.data.error.message
+      });
+    }
   }
 })();
--- www/app/musicsearch/module.js
+++ www/app/musicsearch/module.js
@@ -4,5 +4,20 @@
   angular
     .module('musicHack.musicsearch', [
       'musicHack.common',
-    ]);
+    ])
+    .run(setupYoutube)
+    .config(setupHeaders);
+
+  function setupYoutube() {
+    var tag = document.createElement('script');
+    tag.src = "http://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
+
+  setupHeaders.$inject = ['$httpProvider'];
+
+  function setupHeaders($httpProvider) {
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }
 })();
