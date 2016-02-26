63f579c61755b98983c0dd38f140f08704099868
fsanchez <fsanchez@pernix-solutions.com>
2016-02-26 08:17:35 -0600

    Implementing syncronized array between Firebase and project local array

--- www/app/locals/local/service.js
+++ www/app/locals/local/service.js
@@ -5,26 +5,113 @@
   .module('musicHack.locals')
   .service('VenuePlayerService', VenuePlayerService);
 
-  VenuePlayerService.$inject = ['$rootScope', 'env', '$firebaseArray'];
+  VenuePlayerService.$inject = ['$rootScope', 'env', '$firebaseArray', '$stateParams'];
 
   /* @ngInject */
-  function VenuePlayerService($rootScope, env, $firebaseArray) {
-    this.fetchQueue = fetchQueue;
-    this.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+  function VenuePlayerService($rootScope, env, $firebaseArray, $stateParams) {
+    var service = this;
+    service.fetchQueue = fetchQueue;
+    service.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+    service.getSynchronizedArray = getSynchronizedArray;
+    service.eraseSongFromArray = eraseSongFromArray;
+    service.nowPlaying = {};
 
     var ref = new Firebase(env.firebaseApiUrl);
     var uid = $rootScope.authenticatedUser.uid;
-    var upcomingSongs = [
-      {id: 'LDZX4ooRsWs', title: 'Nicki Minaj - Anaconda'},
-      {id: 'bESGLojNYSo', title: 'Lady Gaga - Poker Face'},
-      {id: '312Sb-2PovA', title: 'SUICIDE SILENCE - You Only Live Once (OFFICIAL VIDEO)'},
-      {id: 'k27N-jRofrM', title: 'SUICIDE SILENCE - Slaves To Substance (OFFICIAL VIDEO)'}
-    ];
+    var upcomingSongs = [];
+    var upcomingFirebaseSongs = [];
     var actualSong = 0;
     var player;
+    var refToQueue;
+    var pathToQueue;
+    var musicFirebaseObject;
 
-    function onYouTubeIframeAPIReady() {
-      var actualVideoId = upcomingSongs[actualSong].id;
+    function eraseSongFromArray(venueInfo, songID) {
+      musicFirebaseObject = getSynchronizedArray(venueInfo);
+      musicFirebaseObject.$remove(songID);
+    }
+
+    function firebaseObjectToArray(venueInfo) {
+      musicFirebaseObject = getSynchronizedArray(venueInfo);
+      upcomingFirebaseSongs = Object.keys(musicFirebaseObject).map(function (key) {return musicFirebaseObject[key]});
+      return upcomingFirebaseSongs;
+    }
+
+    function getSynchronizedArray(venueID) {
+      pathToQueue = "user/" + uid + "/locals/" + venueID + "/queue";
+      refToQueue = ref.child(pathToQueue);
+      var list = [];
+      syncChanges(list, refToQueue);
+      wrapLocalCrudOps(list, refToQueue);
+      return list;
+    }
+
+    function syncChanges(list, ref) {
+      ref.on('child_added', function _add(snap, prevChild) {
+        var data = snap.val();
+        data.$id = snap.key();
+        var pos = positionAfter(list, prevChild);
+        list.splice(pos, 0, data);
+      });
+
+      ref.on('child_removed', function _remove(snap) {
+        var i = positionFor(list, snap.key());
+        if( i > -1 ) {
+          list.splice(i, 1);
+        }
+      });
+
+      ref.on('child_changed', function _change(snap) {
+        var i = positionFor(list, snap.key());
+        if( i > -1 ) {
+          list[i] = snap.val();
+          list[i].$id = snap.key();
+        }
+      });
+
+      ref.on('child_moved', function _move(snap, prevChild) {
+        var curPos = positionFor(list, snap.key());
+        if( curPos > -1 ) {
+          var data = list.splice(curPos, 1)[0];
+          var newPos = positionAfter(list, prevChild);
+          list.splice(newPos, 0, data);
+        }
+      });
+    }
+
+    function positionFor(list, key) {
+      for(var i = 0, len = list.length; i < len; i++) {
+        if( list[i].$id === key ) {
+          return i;
+        }
+      }
+      return -1;
+    }
+
+    function positionAfter(list, prevChild) {
+      if( prevChild === null ) {
+        return 0;
+      }
+      else {
+        var i = positionFor(list, prevChild);
+        if( i === -1 ) {
+          return list.length;
+        }
+        else {
+          return i+1;
+        }
+      }
+    }
+
+    function wrapLocalCrudOps(list, firebaseRef) {
+      list.$remove = function(key) {
+        firebaseRef.child(key).remove();
+      };
+    }
+
+    function onYouTubeIframeAPIReady(venueID) {
+      upcomingSongs = firebaseObjectToArray(venueID);
+      var actualVideoId = upcomingSongs[actualSong].videoID;
       player = new YT.Player('player', {
         videoId: actualVideoId,
         events: {
@@ -32,16 +119,26 @@
           'onStateChange': onPlayerStateChange
         }
       });
+      service.nowPlaying = upcomingSongs[actualSong];
     }
 
     function onPlayerReady(event) {
+      upcomingSongs = firebaseObjectToArray($stateParams.localID);
+      service.nowPlaying = upcomingSongs[actualSong];
+      eraseSongFromArray($stateParams.localID, upcomingSongs[actualSong].videoID);
+      upcomingSongs.splice(0, 1);
       event.target.playVideo();
     }
 
     function onPlayerStateChange(event) {
       if (event.data == YT.PlayerState.ENDED) {
-        actualSong = actualSong + 1;
-        launchNextSong(upcomingSongs[actualSong].id, upcomingSongs[actualSong].title);
+        upcomingSongs = firebaseObjectToArray($stateParams.localID);
+        launchNextSong(upcomingSongs[actualSong].videoID, upcomingSongs[actualSong].name);
+        eraseSongFromArray($stateParams.localID, upcomingSongs[actualSong].videoID);
+        upcomingSongs.splice(0, 1);
+      }
+      if (event.data == YT.PlayerState.PAUSED) {
+        player.clearVideo();
       }
     }
 
@@ -52,9 +149,11 @@
     }
 
     function fetchQueue(localID) {
-      var refToQueue = ref.child("user/" + uid + "/locals/" + localID + "/queue");
+      pathToQueue = "user/" + uid + "/locals/" + localID + "/queue";
+      refToQueue = ref.child(pathToQueue);
       var queue = $firebaseArray(refToQueue);
       return queue.$loaded();
     }
   }
+
 })();
